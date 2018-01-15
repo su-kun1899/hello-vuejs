aa9104ff104ea76f5977b21922be61f5a044dbdf
su-kun1899 <higedrum.coz@gmail.com>
2018-01-15 15:56:23 +0900

    条件分岐とかループとか

--- js/hello.js
+++ js/hello.js
@@ -12,4 +12,45 @@ var app2 = new Vue({
     data: {
         message: 'You loaded this page on ' + new Date().toLocaleString()
     }
-})
+});
+
+// noinspection JSUnresolvedFunction
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+
+// noinspection JSUnresolvedFunction
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+});
+
+// noinspection JSUnresolvedFunction
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
+
+// noinspection JSUnresolvedFunction
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
