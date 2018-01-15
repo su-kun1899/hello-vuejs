58772ef19c8167d7a0c5d505a42d415538ff8999
su-kun1899 <higedrum.coz@gmail.com>
2018-01-15 14:52:43 +0900

    Initial commit

---
+++ js/hello.js
@@ -0,0 +1,15 @@
+// noinspection JSUnresolvedFunction
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+// noinspection JSUnresolvedFunction
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
